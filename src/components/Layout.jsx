cb352de426211978589b1ced371c935e5b273cf6
iam454 <realseogy@gmail.com>
2023-09-26 15:41:34 +0900

    feat: 모바일 화면에 대응할 수 있도록 변경

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,14 @@ import { styled } from "styled-components";
 
 const Container = styled.div`
   width: 390px;
-  height: 787px;
+  height: calc(100vh - 57px);
+  height: calc(100dvh - 57px); /* Mobile */
   background-color: ${(props) => props.theme.black};
   overflow: auto;
   position: relative;
 
   &::-webkit-scrollbar {
-    display: none; /* Chrome, Safari, Opera*/
+    display: none; /* Chrome, Safari, Opera */
   }
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
--- src/pages/PopPage/components/Container.jsx
+++ src/pages/PopPage/components/Container.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { styled } from "styled-components";
 
 const Box = styled.main`
-  width: 350px;
+  width: 358px;
   height: 2000px;
   position: absolute;
-  top: 47px;
+  top: 16px;
   left: 50%;
   transform: translateX(-50%);
   border: 1px solid red;
