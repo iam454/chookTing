dfc1014ca43671101f7ce7416ecbd09622d1c7de
iam454 <realseogy@gmail.com>
2023-10-01 19:24:49 +0900

    test: 스켈레톤 UI 테스트

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import HomeCardSkeleton from "../Skeleton/HomeCardSkeleton";
+import PopCardSkeleton from "../Skeleton/PopCardSkeleton";
+import MyCardSkeleton from "../Skeleton/MyCardSkeleton";
 
 const HomePage = () => {
-
   return (
-  <>
-  <Layout>
-    
-
-
-  </Layout>
-
-  </>);
+    <>
+      <Layout>
+        {/* <HomeCardSkeleton /> */}
+        {/* <PopCardSkeleton /> */}
+        <MyCardSkeleton />
+      </Layout>
+    </>
+  );
 };
 
 export default HomePage;
