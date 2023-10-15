9a01560cc8596e58acff889110a1a9aa85beb768
iam454 <realseogy@gmail.com>
2023-10-15 17:09:24 +0900

    feat: 업로드 페이지용 버튼 생성

--- src/pages/UploadPage/components/Button.jsx
+++ src/pages/UploadPage/components/Button.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const Button = styled.button`
+const StyledButton = styled.button`
   font-family: "Pretendard Variable", Pretendard, -apple-system,
     BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
     "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji",
     "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   cursor: pointer;
-  width: ${(props) => (props.$isLong ? "310px" : "151px")};
+  width: ${(props) => props.$width};
   height: 44px;
   border: none;
   border-radius: 8px;
   outline: none;
-  color: ${(props) =>
-    props.$isTextBlack ? props.theme.black : props.theme.white};
-  background-color: ${(props) => props.$bgColor};
+  font-size: ${(props) => (props.$isReversed ? "11px" : "14px")};
+  color: ${(props) => (props.$isReversed ? "#bebebe" : props.theme.black)};
+  background-color: ${(props) =>
+    props.$isReversed ? "transparent" : props.theme.white};
 `;
 
 const Layout = styled.div`
@@ -32,31 +33,24 @@ const Icon = styled.img`
 
 const Text = styled.div`
   height: 19px;
-  font-size: 14px;
   line-height: 1.4;
 `;
 
-const ModalButton = ({
-  isLong = false,
-  bgColor,
-  isTextBlack = false,
+const Button = ({
   onClick,
+  width = "100%",
+  isReversed = false,
   iconSrc,
   text,
 }) => {
   return (
-    <Button
-      $isLong={isLong}
-      $bgColor={bgColor}
-      $isTextBlack={isTextBlack}
-      onClick={onClick}
-    >
+    <StyledButton onClick={onClick} $width={width} $isReversed={isReversed}>
       <Layout>
         {iconSrc && <Icon src={iconSrc} />}
         <Text>{text}</Text>
       </Layout>
-    </Button>
+    </StyledButton>
   );
 };
 
-export default ModalButton;
+export default Button;
