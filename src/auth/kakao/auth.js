55371f00ae4cf74bdc3539099661fb18e52218c1
iam454 <realseogy@gmail.com>
2023-10-18 19:20:13 +0900

    chore: 환경변수 적용

--- src/auth/kakao/auth.js
+++ src/auth/kakao/auth.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "1234"; // 환경 변수로 관리해야할 것 같습니다
+const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
 const REDIRECT_URI = "http://localhost:3000/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
