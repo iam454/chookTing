63fbf98cf29f481812f536575a675c05ce7ac0ff
iam454 <realseogy@gmail.com>
2023-11-07 17:56:57 +0900

    remove: mydetail 불필요 코드 삭제

--- src/pages/MyDetailpage/MyDetailPage.jsx
+++ src/pages/MyDetailpage/MyDetailPage.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Post from "../../components/Post";
 import PostInfos from "../../components/PostInfos";
 import { useNavigate, useParams } from "react-router-dom";
 import { fetchMyPost } from "../../apis/api/post";
 import { useQuery } from "@tanstack/react-query";
 import { SkeletonPage } from "../SkeletonPage/SkeletonPage";
-
-const sample = {
-  id: 4,
-  image: "/sample.png",
-  createdAt: "2023-09-02",
-  name: "안녕하세요",
-  hashtags: ["해쉬태그", "안녕"],
-  likes: 50,
-  instaViews: 39,
-  isLiked: true,
-};
 
 const MyDetailPage = () => {
   const postId = useParams();
@@ -30,10 +19,7 @@ const MyDetailPage = () => {
       refetch();
       navigate("/profile");
     },
-    cacheTime: 0,
   });
-
-  console.log("myDetail", myDetail);
 
   if (isLoading) {
     return <SkeletonPage.My />;
@@ -42,16 +28,16 @@ const MyDetailPage = () => {
   return (
     <Post.My
       id={myDetail.data.response.postId}
-      image={myDetail?.data.response.imageUrl}
+      image={myDetail.data.response.imageUrl}
       info={
         <PostInfos
           name={myDetail.data.response.nickname}
-          hashtags={myDetail?.data.response.hashTags}
+          hashtags={myDetail.data.response.hashTags}
         />
       }
-      isLikedPost={myDetail?.data.response.isLiked}
-      numberLikes={myDetail?.data.response.likeCount}
-      numberInstas={myDetail?.data.response.viewCount}
+      isLikedPost={myDetail.data.response.isLiked}
+      numberLikes={myDetail.data.response.likeCount}
+      numberInstas={myDetail.data.response.viewCount}
     />
   );
 };
