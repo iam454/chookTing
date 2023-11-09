964d0ff96377b591c156c8f1435c2d88a2927618
iam454 <realseogy@gmail.com>
2023-11-09 15:31:17 +0900

    chore: oauth redirect uri 수정

--- src/auth/instagram/auth.js
+++ src/auth/instagram/auth.js
@@ -1,3 +1,4 @@
-const REDIRECT_URI = "https://localhost:3000/instagram/callback";
+const REDIRECT_URI =
+  "https://k2bf481c846ffa.user-app.krampoline.com/instagram/callback";
 
 export const INSTAGRAM_AUTH_URL = `https://api.instagram.com/oauth/authorize?client_id=${process.env.REACT_APP_INSTAGRAM_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
